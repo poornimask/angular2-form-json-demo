be49bff113836fc76bfa9ac4fa1982274bc1a8f7
poornimask <poornimakulkarni28@gmail.com>
2017-10-23 10:55:01 -0400

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component';
-import {AuthorComponent} from './author.component';
+import {AuthorsComponent} from './author.component';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1> <courses></courses> <authors></authors>',
-    directives : [CoursesComponent, AuthorComponent]
+    directives : [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent { 
 
