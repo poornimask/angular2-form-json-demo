c7e3c87faab260e02bffcdfcd9da6e5df67ea2d6
poornimask <poornimakulkarni28@gmail.com>
2017-10-23 11:28:50 -0400

    Update author.component.ts

--- app/author.component.ts
+++ app/author.component.ts
@@ -1,19 +1,25 @@
 import {Component} from 'angular2/core';
+import {AuthorService} from './author.service';
 
 @Component({
     selector : 'authors',
     template : `
-    <h2>Authors</h2>
-    {{title}}
-    <ul>
-    <li *ngFor="#author of authors">
-    {{author}}
-    </li>
-    </ul>
-    `
+        <h2>Authors</h2>
+        {{title}}
+        <ul>
+        <li *ngFor="#author of authors">
+        {{author}}
+        </li>
+        </ul>
+    `,
+    Providers : [AuthorService]
+    
     
 })
 export class AuthorsComponent{
     title="The title of Author";
-    authors = ["Author1","Author2","Author3"];
+    authors;
+    construtor(authorService: AuthorService)
+    {
+        this.authors = authorService.getAuthors();
 }
