332697009e2d9bdf0043fafc33462dcce469f2b1
poornimask <poornimakulkarni28@gmail.com>
2017-11-02 15:50:09 -0400

    Create app.moule.ts

---
+++ app/app.moule.ts
@@ -0,0 +1,20 @@
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
